4b4c69937df76d55a57a186f6c70ec750118f8b2
lemuelmc <lemuel.raganas@mediacorp.com.sg>
2017-05-17 17:18:28 +0800

    WIP

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -4,6 +4,9 @@
 // importScripts('/__/firebase/3.9.0/firebase-app.js');
 // importScripts('/__/firebase/3.9.0/firebase-messaging.js');
 // importScripts('/__/firebase/init.js');
+importScripts('firebase/3.9.0/firebase-app.js');
+importScripts('firebase/3.9.0/firebase-messaging.js');
+// importScripts('/firebase/init.js');
 
 const messaging = firebase.messaging();
 
